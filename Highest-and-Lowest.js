f858b26f763d4bbb133ed22ecd29d58ae55199df
passing-j <jiangbeef@163.com>
2017-05-31 10:49:02 +0800

    update comments

--- Highest-and-Lowest.js
+++ Highest-and-Lowest.js
@@ -1,6 +1,7 @@
 // 输出最大值和最小值
 function highAndLow(numbers) {
-  var arr = numbers.split(' ')
+  // 转成数值数组
+  var arr = numbers.split(' ').map(Number)
   return `${Math.max.call(null, ...arr)} ${Math.min.apply(null, arr)}`
 }
 console.log(highAndLow("1 2 -4 5"))
