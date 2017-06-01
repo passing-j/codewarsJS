09669ebe49ad7d68b538e1b704906a2291eae052
passing-j <jiangbeef@163.com>
2017-06-01 11:34:01 +0800

    modify character

--- output-names.js
+++ output-names.js
@@ -26,7 +26,7 @@ function likes(names) {
     1: `${names[0]} likes this`,
     2: `${names[0]} and ${names[1]} like this`,
     3: `${names[0]}, ${names[1]} and ${names[2]} like this`,
-    4: `${names[0]}, ${names[1]} and ${names.length - 2} others like this`,
+    4: `${names[0]}, ${names[1]} and ${names.length - 2} others like this`
   }[Math.min(4, names.length)]
 }
 
