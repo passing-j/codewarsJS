debc4832a21156c1a20062d578d8f55bdd70130b
passing-j <jiangbeef@163.com>
2017-06-05 11:15:01 +0800

    Add another way

--- replace-and-delete-space.js
+++ replace-and-delete-space.js
@@ -4,4 +4,10 @@ function songDecoder(song) {
   return song.replace(/(WUB)+/g, ' ').trim()
 }
 
+// method2
+function songDecoder(song){
+  return song.split('WUB').filter(Boolean).join(' ');
+}
+
+
 console.log(songDecoder("AWUBWUBWUBBWUBWUBWUBC"))
